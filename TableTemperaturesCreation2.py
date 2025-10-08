14a83107b30b9a429d123fda98cb38928b431333
jpfnice <jpfnice@gmail.com>
2025-10-08 10:11:24 +0200

    Add files via upload

--- TableTemperaturesCreation2.py
+++ TableTemperaturesCreation2.py
@@ -1,12 +1,13 @@
 """
-GRANT
+To create the table temperatures (if it already exists, 
+                                  it gonna first be dropped)
 
 """
 import sqlite3
 
 try:
 
-    with sqlite3.connect("pyInt.db") as conn:
+    with sqlite3.connect("epfl_2025.db") as conn:
         cursor=conn.cursor()
         cursor.execute("drop table if exists temperatures")
         cursor.execute("create table temperatures (city varchar(20), time time, date date, temp float)")
